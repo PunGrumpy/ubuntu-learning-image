f18e6a944f4b7811b87df2be8cd582a14a96f265
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-04-23 08:31:29 +0700

    fix(welcome): enhance more user-friendly

--- scripts/welcome.sh
+++ scripts/welcome.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 clear
-echo "Welcome $USER to Ubuntu $(lsb_release -rs) (GNU/Linux $(uname -r))"
+
+echo "Welcome, $USER, to Ubuntu $(lsb_release -ds)"
 echo
+
+echo "Helpful Links:"
 echo " * Documentation:  https://help.ubuntu.com"
 echo " * Management:     https://landscape.canonical.com"
 echo " * Support:        https://ubuntu.com/advantage"
 echo
-echo "  System information as of $(date)"
+
+echo "System Information as of $(date):"
 echo
 landscape-sysinfo || echo "Error: Unable to retrieve system information."
 echo
+
 updates=$(apt list --upgradable 2>/dev/null | grep -c '/')
 if [[ $updates -gt 0 ]]; then
-  echo "$updates updates can be applied."
+  echo "$updates updates are available."
 else
   echo "No updates available."
 fi
-echo "If you are new to Linux, you can play a game to learn the basics. (Type 'game_beginner' to play)"
+echo
+
+echo "If you're new to Linux, you can play a game to learn the basics. (Type 'game_beginner' to play)"
